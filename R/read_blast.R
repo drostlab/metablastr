799210eeee8bf5a750fe804de24aa460addc579b
HajkD <hgd23@cam.ac.uk>
2017-04-26 17:00:48 +0100

    add functionality for reading tab separated BLAST output

--- R/read_blast.R
+++ R/read_blast.R
@@ -2,6 +2,7 @@
 #' @description When performing BLAST searches with the \code{blast_*()} functions,
 #' the corresponding BLAST output file can be imported into the current R session using this function.
 #' All output formats given by BLAST are supported (see e.g. description for details).
+#' @param file path to BLAST output file.
 #' @param out.format a character string specifying the format of the file in which the BLAST results shall be stored.
 #' Available options are:
 #'  \itemize{
@@ -24,12 +25,60 @@
 #'  \item \code{out.format = "xml2.blast.single"} : Single-file BLAST XML2
 #'  \item \code{out.format = "report"} : Organism Report
 #'  }
+#'  @param spark_version specify Spark version that shall be installed locally. Default is \code{spark_version = "2.0.1"}.
 #'  @author Hajk-Georg Drost
 #'  @seealso \code{\link{blast_protein}}
 #'  @export
-read_blast <- function(out.format) {
+read_blast <- function(file, out.format, spark_version = "2.0.1") {
 
-        
+   if (out.format == "tab") {
+       
+       # # install local version of Spark if not available yet
+       # if (nrow(sparklyr::spark_installed_versions()) == 0) {
+       #     sparklyr::spark_install(version = spark_version)
+       # }
+       # 
+       # # open local connection to spark
+       # sparkconnect <- sparklyr::spark_connect(master = "local")
+       # 
+       # import_blast_tbl <- sparklyr::copy_to(sparkconnect, iris, 
+       #                                       "spark_blast_tbl",
+       #                                        overwrite = TRUE) 
+       
+       
+       
+       
+       
+       sqlite3_filename <- paste0(basename(file),".sqlite3")
+       
+       blast_sql_db <-
+           dplyr::src_sqlite(sqlite3_filename, create = TRUE)
+       
+       connect_db <-
+           DBI::dbConnect(RSQLite::SQLite(), dbname = sqlite3_filename)
+       
+       
+       DBI::dbWriteTable(
+           connect_db,
+           name      = paste0(basename(file),"._blast_hit_tbl"),
+           value     = file,
+           row.names = FALSE,
+           header    = FALSE,
+           sep       = "\t",
+           overwrite = TRUE
+       )
+       
+       on.exit({
+           #sparklyr::spark_disconnect(sparkconnect)
+           DBI::dbDisconnect(connect_db)
+           
+       })
+       
+       blast_sqlite <-
+           dplyr::tbl(blast_sql_db, paste0(basename(file),"._blast_hit_tbl"))
+       
+       return(blast_sqlite)
+   }     
     
     
     

4f893e5d0042e94b31b6423b6f878098eb14f310
HajkD <hgd23@cam.ac.uk>
2017-04-27 18:20:48 +0100

    add BLAST params function

---
+++ R/blast_outfmt_colnames.R
@@ -0,0 +1,29 @@
+blast_outfmt_colnames <- function() {
+    
+    params <-
+        c(
+            "query_id",
+            "subject_id",
+            "perc_identity",
+            "num_ident_matches",
+            "alig_length",
+            "mismatches",
+            "gap_openings",
+            "n_gaps",
+            "pos_match",
+            "ppos",
+            "q_start",
+            "q_end",
+            "q_len",
+            "qcov",
+            "qcovhsp",
+            "s_start",
+            "s_end",
+            "s_len",
+            "evalue",
+            "bit_score",
+            "score_raw"
+        )
+    
+    return(params)
+}

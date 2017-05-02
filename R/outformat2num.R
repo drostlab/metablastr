f526a52ab9c6ea49fbd1cc1a50dd8801b34a7d4e
HajkD <hgd23@cam.ac.uk>
2017-05-02 10:36:03 +0100

    add new out format for blastn

--- R/outformat2num.R
+++ R/outformat2num.R
@@ -2,26 +2,28 @@
 # used as input by the BLAST command line tool
 outformat2num <- function(out.format) {
     if (is_outformat(out.format)) {
-        return(switch(out.format,
-               pair = 0,
-               qa.ident = 1,
-               qa.nonident = 2,
-               fq.ident = 3,
-               fq.nonident = 4,
-               xml = 5,
-               tab = 6,
-               tab.comment = 7,
-               ASN.1.text = 8,
-               ASN.1.binary = 9,
-               csv = 10,
-               ASN.1 = 11,
-               json.seq.aln = 12,
-               json.blast.multi = 13,
-               xml2.blast.multi = 14,
-               json.blast.single = 15,
-               xml2.blast.single = 16,
-               report = 18
-               ))
+        return(switch(
+            out.format,
+            pair = 0,
+            qa.ident = 1,
+            qa.nonident = 2,
+            fq.ident = 3,
+            fq.nonident = 4,
+            xml = 5,
+            tab = 6,
+            tab.comment = 7,
+            ASN.1.text = 8,
+            ASN.1.binary = 9,
+            csv = 10,
+            ASN.1 = 11,
+            json.seq.aln = 12,
+            json.blast.multi = 13,
+            xml2.blast.multi = 14,
+            json.blast.single = 15,
+            xml2.blast.single = 16,
+            SAM = 17,
+            report = 18
+        ))
         
     }
 }

ba848473d25859553750a9b48e55af0a4b7fbeaf
HajkD <hajkdrost@googlemail.com>
2017-11-27 16:11:38 +0000

    update default parameters

--- R/read_blast.R
+++ R/read_blast.R
@@ -3,11 +3,12 @@
 #' the corresponding BLAST output file can be imported into the current R session using this function.
 #' All output formats given by BLAST are supported (see e.g. description for details).
 #' @param file path to BLAST output file.
-#' @param out.format a character string specifying the format of the file in which the BLAST results shall be stored.
+#' @param out.format a character string specifying the output format of the BLAST output that shall be imported.
 #' Available options are:
 #'  \itemize{
+#'  \item \code{out.format = "postgres"} : store BLAST output as Postgres database and generate postgres database connection.
 #'  \item \code{out.format = "xml"} : XML
-#'  \item \code{out.format = "tab"} : Tabular separated file
+#'  \item \code{out.format = "tsv"} : Tabular separated file
 #'  \item \code{out.format = "csv"} : Comma-separated values
 #'  \item \code{out.format = "json.seq.aln"} : Seqalign (JSON)
 #'  \item \code{out.format = "json.blast.multi"} : Multiple-file BLAST JSON
@@ -15,14 +16,36 @@
 #'  \item \code{out.format = "json.blast.single"} : Single-file BLAST JSON
 #'  \item \code{out.format = "xml2.blast.single"} : Single-file BLAST XML2
 #'  }
-#'  @param postgres.user specify Spark version that shall be installed locally. Default is \code{spark_version = "2.0.1"}.
+#'  @param postgres.user specify username for RPostgreSQL connection.
 #'  @author Hajk-Georg Drost
-#'  @seealso \code{\link{blast_protein}}
+#'  @seealso \code{\link{blast_protein_to_protein}}, \code{\link{blast_nucleotide_to_protein}},
+#'  \code{\link{blast_nucleotide_to_nucleotide}}
 #'  @importFrom RPostgreSQL
 #'  @export
+
 read_blast <- function(file, out.format, postgres.user = NULL) {
 
-   if (out.format == "tab") {
+  if (!file.exists(file))
+    stop("The BLAST output file '", file, "' does not exist! Please check what might have went wrong with the BLAST call.", call. = FALSE)
+  
+  if (!is.element(
+    out.format,
+    c(
+      "postgres",
+      "xml",
+      "tsv",
+      "csv",
+      "json.seq.aln",
+      "json.blast.multi",
+      "xml2.blast.multi",
+      "json.blast.single",
+      "xml2.blast.single"
+    )
+  )
+  )
+  stop("Sorry, but '",out.format,"' is not an available import type. Please choose an 'out.format' that is supported by this function.", call. = FALSE)
+  
+   if (out.format == "postgres") {
        
        # # install local version of Spark if not available yet
        # if (nrow(sparklyr::spark_installed_versions()) == 0) {
@@ -38,7 +61,7 @@ read_blast <- function(file, out.format, postgres.user = NULL) {
        
        
        if (is.null(postgres.user))
-           stop("Please specify a 'postgres.user' to import BLAST output into PostgresSQL database.")
+           stop("Please specify a 'postgres.user' to import BLAST output into PostgresSQL database.", call. = FALSE)
        
        require(RPostgreSQL)
      
@@ -86,7 +109,7 @@ read_blast <- function(file, out.format, postgres.user = NULL) {
     
    
     if (out.format == "csv") {
-        blast_csv <- readr::read_delim(file, delim = ",", 
+        blast_csv <- readr::read_delim(file = file, delim = ",", 
                                        col_names = FALSE,
                                        col_types = readr::cols(
                                            "X1" = readr::col_character(),

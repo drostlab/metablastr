f6e0cfa69d701ce4340bedba3ae740463e2fd7e2
Anna Gogleva <gogleva.a.a@gmail.com>
2018-04-23 16:38:24 +0100

    rpsblast to do: db prep and tibble conversion for output

--- R/blast_rpsblast.R
+++ R/blast_rpsblast.R
@@ -10,38 +10,45 @@
 #' PostgresSQL database. In that case, users need to have PostgresSQL installed and initialized on their system. 
 #' Please consult the Installation Vignette for details. 
 #' @param evalue Expectation value (E) threshold for saving hits (default: \code{evalue = 0.001}).
-#' @param out.format a character string specifying the format of the file in which the BLAST results shall be stored.
+#' @param out.format a character string specifying the format of the file in which the rpsblast results shall be stored.
+#' Available options are:
+#'  \itemize{
+#'  \item \code{out.format = "pair"} : Pairwise
+#'  \item \code{out.format = "qa.ident"} : Query-anchored showing identities
+#'  \item \code{out.format = "qa.nonident"} : Query-anchored no identities
+#'  \item \code{out.format = "fq.ident"} : Flat query-anchored showing identities
+#'  \item \code{out.format = "fq.nonident"} : Flat query-anchored no identities
+#'  \item \code{out.format = "xml"} : XML
+#'  \item \code{out.format = "tab"} : Tabular separated file
+#'  \item \code{out.format = "tab.comment"} : Tabular separated file with comment lines
+#'  \item \code{out.format = "ASN.1.text"} : Seqalign (Text ASN.1)
+#'  \item \code{out.format = "ASN.1.binary"} : Seqalign (Binary ASN.1)
+#'  \item \code{out.format = "csv"} : Comma-separated values
+#'  \item \code{out.format = "ASN.1"} : BLAST archive (ASN.1)
+#'  \item \code{out.format = "json.seq.aln"} : Seqalign (JSON)
+#'  \item \code{out.format = "json.blast.multi"} : Multiple-file BLAST JSON
+#'  \item \code{out.format = "xml2.blast.multi"} : Multiple-file BLAST XML2
+#'  }
 #' @param cores number of cores for parallel rpsblast searches.
 #' @param blast.path path to rpsblast executables.
 #' @author Hajk-Georg Drost, Anna Gogleva
 #' @seealso \code{\link{blast_protein_to_protein}}, \code{\link{blast_nucleotide_to_protein}}
 #' @export
 #' @examples
-#' small_fasta <- '/home/anna/anna/Labjournal/SecretSanta_external/test_fastas/small_10.fasta'
 #' db_path <- '/home/anna/anna/Labjournal/db_for_protoro'
 #' db_alias <- 'myCdd'
-#' result <- blast_rpsblast(query = small_fasta, db = db_path, db.alias = db_alias)
-#' 
+#' rps_test <- blast_rpsblast(query = system.file('seqs/sbj_aa.fa', package = 'metablastr'), db = db_path, db.alias = db_alias, output.path = tempdir())
 
-query = small_fasta
-db = db_path
-db.alias = db_alias
-output.path = NULL
-db.import = FALSE
-postgress.user = NULL
-evalue = 1E-3
-out.format = 8
-cores = 1
-blast.path = NULL
+query = system.file('seqs/sbj_aa.fa', package = 'metablastr')
 
 blast_rpsblast <- function(query,
                            db,
                            db.alias,
                            output.path = NULL,
                            db.import = FALSE,
-                           postgress.user = NULL,
+                           postgres.user = NULL,
                            evalue = 1E-3,
-                           out.format = 8,
+                           out.format = 'csv',
                            cores = 1,
                            blast.path = NULL) {
     
@@ -80,32 +87,46 @@ blast_rpsblast <- function(query,
         stop('Unfortunately, one ore more required database files are missing: ', missings, call. = FALSE)
     }
     
-    message("Starting 'rpsblast","' with  query: ", query, " against database: ", db.alias," using ", cores, " core(s) ...")
+    message("Starting 'rpsblast+","' with  query: ", query, " against database: ", db.alias," using ", cores, " core(s) ...")
     
-    # rpasblast calls here
+    # rpasblast call starts here
     
-    rpsblast_call <- paste('rpsblast -i',
+    rpsblast_call <- paste('rpsblast+ -query',
                            query,
-                           '-d', 
+                           '-db', 
                            full.db.path
                            )
-    # this does not work yet
+  
+    # output file with ws handling
     
-    # output_rpsblast <-
-    #     file.path(ifelse(is.null(output.path), ws_wrap(getwd()), ws_wrap(output.path)),
-    #               paste0(unlist(stringr::str_split(
-    #                   basename(query), "[.]"
-    #               ))[1], ".blast_tbl"))
+    output_rpsblast <-
+        file.path(ifelse(is.null(output.path), ws_wrap(getwd()), ws_wrap(output.path)),
+                  paste0(unlist(stringr::str_split(
+                      basename(query), "[.]"
+                  ))[1], ".rpsblast_tbl"))
     
-    # the call itself
+    # don't need ws handling anymore when reading file
+    output_read_rpsblast <-
+        file.path(ifelse(is.null(output.path), getwd(), output.path),
+                  paste0(unlist(stringr::str_split(
+                      basename(query), "[.]"
+                  ))[1], ".rpsblast_tbl"))
+    
+    # prep database if not ready yet (to do)
+    
+    
+    # the rpsblast call itself
     system(
         paste0(
             ifelse(is.null(blast.path), rpsblast_call, paste0("export PATH=$PATH:", rpsblast_call)),
-               ' -e ',
+               ' -evalue ',
                evalue,
-               " -m 8 ",
-               ' -a ',
-               cores
+               paste0(' -outfmt ', outformat2num(out.format = out.format)),
+               ' -num_threads ',
+               cores,
+               ' -out ',
+               output_rpsblast
                ))
-        
+    
+    # read rpasblast outputs and convert into tibble (to do)
 }

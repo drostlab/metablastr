801090b996f112c9de0c7aaabf6dec6e1fba6597
Anna Gogleva <gogleva.a.a@gmail.com>
2018-04-23 15:14:30 +0100

    blast_rpsblast: basic functionality works, outformats need attention

--- R/blast_rpsblast.R
+++ R/blast_rpsblast.R
@@ -11,33 +11,28 @@
 #' Please consult the Installation Vignette for details. 
 #' @param evalue Expectation value (E) threshold for saving hits (default: \code{evalue = 0.001}).
 #' @param out.format a character string specifying the format of the file in which the BLAST results shall be stored.
-#' Available options are:
-#'  \itemize{
-#'  \item \code{out.format = "pair"} : Pairwise
-#'  \item \code{out.format = "qa.ident"} : Query-anchored showing identities
-#'  \item \code{out.format = "qa.nonident"} : Query-anchored no identities
-#'  \item \code{out.format = "fq.ident"} : Flat query-anchored showing identities
-#'  \item \code{out.format = "fq.nonident"} : Flat query-anchored no identities
-#'  \item \code{out.format = "xml"} : XML
-#'  \item \code{out.format = "tab"} : Tabular separated file
-#'  \item \code{out.format = "tab.comment"} : Tabular separated file with comment lines
-#'  \item \code{out.format = "ASN.1.text"} : Seqalign (Text ASN.1)
-#'  \item \code{out.format = "ASN.1.binary"} : Seqalign (Binary ASN.1)
-#'  \item \code{out.format = "csv"} : Comma-separated values
-#'  \item \code{out.format = "ASN.1"} : BLAST archive (ASN.1)
-#'  \item \code{out.format = "json.seq.aln"} : Seqalign (JSON)
-#'  \item \code{out.format = "json.blast.multi"} : Multiple-file BLAST JSON
-#'  \item \code{out.format = "xml2.blast.multi"} : Multiple-file BLAST XML2
-#'  \item \code{out.format = "json.blast.single"} : Single-file BLAST JSON
-#'  \item \code{out.format = "xml2.blast.single"} : Single-file BLAST XML2
-#'  \item \code{out.format = "SAM"} : Sequence Alignment/Map (SAM)
-#'  \item \code{out.format = "report"} : Organism Report
-#'  }
 #' @param cores number of cores for parallel rpsblast searches.
-#' @param blast.path path to BLAST executables.
-#' @author Anna Gogleva
+#' @param blast.path path to rpsblast executables.
+#' @author Hajk-Georg Drost, Anna Gogleva
 #' @seealso \code{\link{blast_protein_to_protein}}, \code{\link{blast_nucleotide_to_protein}}
 #' @export
+#' @examples
+#' small_fasta <- '/home/anna/anna/Labjournal/SecretSanta_external/test_fastas/small_10.fasta'
+#' db_path <- '/home/anna/anna/Labjournal/db_for_protoro'
+#' db_alias <- 'myCdd'
+#' result <- blast_rpsblast(query = small_fasta, db = db_path, db.alias = db_alias)
+#' 
+
+query = small_fasta
+db = db_path
+db.alias = db_alias
+output.path = NULL
+db.import = FALSE
+postgress.user = NULL
+evalue = 1E-3
+out.format = 8
+cores = 1
+blast.path = NULL
 
 blast_rpsblast <- function(query,
                            db,
@@ -46,7 +41,7 @@ blast_rpsblast <- function(query,
                            db.import = FALSE,
                            postgress.user = NULL,
                            evalue = 1E-3,
-                           out.format = 'csv',
+                           out.format = 8,
                            cores = 1,
                            blast.path = NULL) {
     
@@ -70,24 +65,47 @@ blast_rpsblast <- function(query,
         stop("Unfortunately, no query file has been found at ", query, call. = FALSE)
     
     # test if all required database files exist
-    db_postfix <- c('aux', 'freq', 
+    db.postfix <- c('aux', 'freq', 
                     'loo', 'phr',
                     'pin', 'psi',
                     'psq', 'rps')
 
-    full_path <- paste(db, db.alias, sep = '/')
-    db_files <- sapply(db_postfix,
-                       function(x) paste(full_path, x, sep = '.'))
-    db_status <- sapply(db_files, file.exists)
+    full.db.path <- paste(db, db.alias, sep = '/')
+    db.files <- sapply(db.postfix,
+                       function(x) paste(full.db.path, x, sep = '.'))
+    db.status <- sapply(db.files, file.exists)
     
-    if (!(all(db_stats))) {
-        stop('Unfortunately, one ore more required database files are missing', names(db_status[db_status == FALSE]) call. = FALSE)
+    if (!(all(db.status))) {
+        missings <- paste(names(db.status[db.status == FALSE]), collapse = ' ')
+        stop('Unfortunately, one ore more required database files are missing: ', missings, call. = FALSE)
     }
     
     message("Starting 'rpsblast","' with  query: ", query, " against database: ", db.alias," using ", cores, " core(s) ...")
     
     # rpasblast calls here
     
+    rpsblast_call <- paste('rpsblast -i',
+                           query,
+                           '-d', 
+                           full.db.path
+                           )
+    # this does not work yet
     
+    # output_rpsblast <-
+    #     file.path(ifelse(is.null(output.path), ws_wrap(getwd()), ws_wrap(output.path)),
+    #               paste0(unlist(stringr::str_split(
+    #                   basename(query), "[.]"
+    #               ))[1], ".blast_tbl"))
     
+    # the call itself
+    system(
+        paste0(
+            ifelse(is.null(blast.path), rpsblast_call, paste0("export PATH=$PATH:", rpsblast_call)),
+               ' -e ',
+               evalue,
+               " -m 8 ",
+               ' -a ',
+               cores
+               ))
+        
 }

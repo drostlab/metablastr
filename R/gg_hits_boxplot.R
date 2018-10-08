7a579b83254d8d1b879720a61f6c9f543da9f009
HajkD <hajkdrost@googlemail.com>
2018-10-08 16:08:18 +0100

    update to more visual boxplot

--- R/gg_hits_boxplot.R
+++ R/gg_hits_boxplot.R
@@ -14,6 +14,7 @@
 #' }
 #' @param xlab x-axis label.
 #' @param ylab y-axis label.
+#' @param title plot title.
 #' @param text_size size of label text.
 #' @param y_ticks number of ticks on the y-axis.
 #' @author Hajk-Georg Drost
@@ -25,6 +26,7 @@ gg_hits_boxplot <-
            y_type = "qcovhsp",
            xlab = x_type,
            ylab = y_type,
+           title = "",
            text_size = 18,
            y_ticks = 8) {
   
@@ -36,10 +38,12 @@ gg_hits_boxplot <-
       stop("Please choose a 'y_type' that is available in the 'blast_tbl': ",names(blast_tbl), call. = FALSE)
     
     
-  p <- ggplot2::ggplot(blast_tbl, ggplot2::aes_string(x = x_type, y = y_type, fill = x_type))
+  p <- ggplot2::ggplot(blast_tbl, ggplot2::aes_string(x = x_type, y = y_type, colour = x_type))
     
   p <- p +  
-    ggplot2::geom_boxplot()  +
+    ggplot2::geom_boxplot(size = 1.5, alpha = 0.7)  +
+    ggplot2::geom_point(ggplot2::aes_string(colour = x_type), alpha = 0.3) +
+    ggplot2::geom_jitter(ggplot2::aes_string(colour = x_type), position = ggplot2::position_jitter(0.25), alpha = 0.3) +
     ggplot2::theme_minimal() +
     ggplot2::labs(x = xlab,
                   y = ylab) +
@@ -58,9 +62,9 @@ gg_hits_boxplot <-
       angle = 90,
       vjust = 1,
       hjust = 1
-    )) +
+    )) + ggplot2::ggtitle(paste0(title, " (total: ",nrow(blast_tbl)," hits)")) +
     ggplot2::scale_y_continuous(breaks = scales::pretty_breaks(n = y_ticks)) +
-    ggsci::scale_fill_lancet()
+    ggsci::scale_colour_lancet()
   
   return(p)
 }

a317d4bf75c497f1561dc417e413034449f8fb53
HajkD <hgd23@cam.ac.uk>
2019-02-15 18:17:04 +0000

    add new function for sequence type check

---
+++ R/file_contains_aa.R
@@ -0,0 +1,30 @@
+#' @title Helper function to check whether input file contains amino acids as required
+#' @description Check whether input file contains amino acids as required.
+#' @param file file path to \code{fasta} file.
+#' @param input_type a character string specifying whether input file a \code{"query"} or \code{"subject"} file.
+#' @author Hajk-Georg Drost
+#' @return When a file contains the correct sequence type then this function
+#' will only execute without any output. In case a file does not contains the 
+#' correct sequence type then an error is thrown.
+#' @examples 
+#' # test an aa file
+#' file_contains_aa(file = system.file('seqs/qry_aa.fa', package = 'metablastr'), input_type = "query")
+#' @export
+file_contains_aa <- function(file, input_type) {
+  if (!is.element(input_type, c("query", "subject")))
+    stop("Please provide a valid input_type.", call. = FALSE)
+  
+  alphabet <- Biostrings::uniqueLetters(Biostrings::readBStringSet(filepath = file, nrec = 1))
+  if (any((!is.element(stringr::str_to_upper(alphabet), Biostrings::AA_ALPHABET))))
+    stop("Please provide the correct sequence type for the ", input_type, " file. The correct sequence type is: AMINO ACIDS.", call. = FALSE)
+  
+  if (any(!is.element(c("Q", "E"), stringr::str_to_upper(alphabet))))
+    stop("Please provide the correct sequence type for the ", input_type, " file. The correct sequence type is: AMINO ACIDS.", call. = FALSE)
+}
+
+
+
+
+
+
+

53cbd5b0e10241362d7fb23f93ab5f9349650f29
HajkD <hgd23@cam.ac.uk>
2019-05-30 12:56:38 +0100

    fix global variable assignment and update docu

--- R/filter_homologs_core_set.R
+++ R/filter_homologs_core_set.R
@@ -2,22 +2,25 @@
 #' @description This function aims to retrieve a core set of best blast hits for each query sequence that is shared across all species. In other words, only query sequences
 #' that generated blast hits in all species in the input \code{blast_tbl} were retained.
 #' by filtering a \code{blast_tbl} using the following criteria.
-#' A best hit is defined as (fulfilling all three critaria):
+#' A best hit is defined as (fulfilling all three critaria)
 #' \itemize{
 #' \item max(alig_length): only the hit having the longest alignment length is retained.
 #' \item \code{qcovhsp >= min_qcovhsp}: only hits that have a query coverage of at least \code{min_qcovhsp} are retained.
 #' \item max(bit_score): only the hit having the highest bit-score is retained.
 #' }
 #' @param blast_tbl a BLAST table generated with \code{\link{detect_homologs_proteome_to_proteome}} or \code{\link{detect_homologs_cds_to_cds}}.
-#' @param min_qcovhsp minimum query coverage of the hit in percent \code{0..100} that shall be retained. Default value is set to \code{min_qcovhsp = 50} (= a best hit alignment must have at least 50% query coverage).
+#' @param min_qcovhsp minimum query coverage of the hit in percent \code{0..100} that shall be retained. Default value is set to \code{min_qcovhsp = 50} (= a best hit alignment must have at least 50 percent query coverage).
 #' @author Hajk-Georg Drost
 #' @seealso \code{\link{filter_best_hits}}
 #' @export
 #'
 filter_homologs_core_set <- function(blast_tbl, min_qcovhsp = 50) {
+  
   message("Retrieve core set of homologs (= query hits shared across all species).")
-  qcovhsp <- query_id <- NULL
-  blast_tbl_processed <- filter(blast_tbl, qcovhsp >= min_qcovhsp)
+  
+  qcovhsp <- query_id <- '.' <- NULL
+  
+  blast_tbl_processed <- dplyr::filter(blast_tbl, qcovhsp >= min_qcovhsp)
   
   # total number of species in the BLAST table
   total_n_species <-
@@ -49,7 +52,7 @@ filter_homologs_core_set <- function(blast_tbl, min_qcovhsp = 50) {
     }
   }
   
-  query_id <- NULL
+  query_id <- gene_name <- NULL
   blast_tbl_processed_hit_in_all_species <-
     dplyr::do(dplyr::group_by(blast_tbl_processed, gene_name),
               filter_species_hits(.))

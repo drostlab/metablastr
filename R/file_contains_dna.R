4215be236b645df457ccae4737fb104275e2a00c
HajkD <hajkdrost@googlemail.com>
2019-07-01 17:58:56 +0100

    fixing bug that caused non-functional blasting against subject genomes when they contained NNN in sequences

--- R/file_contains_dna.R
+++ R/file_contains_dna.R
@@ -4,11 +4,11 @@
 #' @param input_type a character string specifying whether input file a \code{"query"} or \code{"subject"} file.
 #' @author Hajk-Georg Drost
 #' @return When a file contains the correct sequence type then this function
-#' will only execute without any output. In case a file does not contains the 
+#' will only execute without any output. In case a file does not contains the
 #' correct sequence type then an error is thrown.
-#' @examples 
+#' @examples
 #' # test an dna file
-#' file_contains_dna(file = system.file('seqs/qry_nn.fa', package = 'metablastr'), 
+#' file_contains_dna(file = system.file('seqs/qry_nn.fa', package = 'metablastr'),
 #'                   input_type = "query")
 #' @export
 
@@ -16,13 +16,23 @@ file_contains_dna <- function(file, input_type) {
   if (!is.element(input_type, c("query", "subject")))
     stop("Please provide a valid input_type.", call. = FALSE)
   
-  alphabet <- Biostrings::uniqueLetters(Biostrings::readBStringSet(filepath = file, nrec = 1))
-  if (any(!is.element(stringr::str_to_upper(alphabet), Biostrings::DNA_ALPHABET)))
-    stop("Please provide the correct sequence type for the ", input_type, " file. The correct sequence type is: DNA.", call. = FALSE)
+  alphabet <-
+    paste0(unique(stringr::str_to_upper(Biostrings::uniqueLetters(
+      Biostrings::readBStringSet(filepath = file, nrec = 1)
+    ))), collapse = "")
   
-  if (paste0(stringr::str_to_upper(alphabet)[1:4], collapse = "") != paste0(Biostrings::DNA_ALPHABET[1:4], collapse = ""))
-    stop("Please provide the correct sequence type for the ", input_type, " file. The correct sequence type is: DNA.", call. = FALSE)
+    if (!any(is.element(alphabet, c("ACGNT", "ACGT")))) {
+      message("Your ",
+              input_type,
+              " file contains the folowing letters: '",
+              alphabet,
+              "'.")
+      message("However, these letters shoould be present: ", "'ACGT' or 'ACGNT'", ".")
+      stop(
+        "Please provide the correct sequence type for the ",
+        input_type,
+        " file. The correct sequence type is: DNA.",
+        call. = FALSE
+      )
+    }
 }
-
-
-

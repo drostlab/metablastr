ac6fb46ade571912c142adeeef63744885ad6f57
HajkD <hajkdrost@googlemail.com>
2019-12-21 14:25:54 +0100

    update unit test

--- tests/testthat/test-file_contains.R
+++ tests/testthat/test-file_contains.R
@@ -6,7 +6,7 @@ test_that("file_contains_aa() works properly with input aa fasta file ...", {
 })
 
 test_that("file_contains_aa() throws error when input file contains dna rather than aa ...", {
-  expect_error(file_contains_aa(system.file('seqs/qry_nn.fa', package = 'metablastr'), "query"))
+  expect_warning(file_contains_aa(system.file('seqs/qry_nn.fa', package = 'metablastr'), "query"))
   
 })
 
@@ -16,7 +16,7 @@ test_that("file_contains_dna() works properly with input dna fasta file ...", {
 })
 
 test_that("file_contains_dna() throws error when input file contains aa rather than dna ...", {
-  expect_error(file_contains_dna(system.file('seqs/qry_aa.fa', package = 'metablastr'), "query")
+  expect_warning(file_contains_dna(system.file('seqs/qry_aa.fa', package = 'metablastr'), "query")
 )
   
 })
